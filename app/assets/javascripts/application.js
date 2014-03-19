30c8131a33d21f42883ef98fe05f8395596615f9
ryysd <ry.ysd01@gmail.com>
2014-03-20 00:37:32 +0900

    modify config and add locale

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+//= require jquery
+//= require jquery.turbolinks
+//= require turbolinks
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-AppRanking::Application.config.secret_key_base = 'fdc51a2af34682aa0296124c729380b3b1a443ff437d88dddffe738b51c76eee82e91fbcfe7c0f849fde19abbc79e327073cb0a656d07e27ec8e3de0f537e533'
+AppRanking::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || 'fdc51a2af34682aa0296124c729380b3b1a443ff437d88dddffe738b51c76eee82e91fbcfe7c0f849fde19abbc79e327073cb0a656d07e27ec8e3de0f537e533'
