13a07f93cd63be4f3ad249a0d1f73c6d8ccc0d02
ryysd <ry.ysd01@gmail.com>
2014-03-24 13:22:07 +0900

    add proxy model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140324033911) do
+ActiveRecord::Schema.define(version: 20140324042007) do
 
   create_table "app_items", force: true do |t|
     t.integer  "ranking_id",                                  null: false
@@ -117,6 +117,13 @@ ActiveRecord::Schema.define(version: 20140324033911) do
   end
 
   add_index "prices", ["id"], name: "id_UNIQUE", unique: true, using: :btree
+
+  create_table "proxies", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "country_id",            null: false
+    t.string   "ip_address", limit: 64, null: false
+  end
 
   create_table "publishers", force: true do |t|
     t.string  "name",      limit: 64, null: false
