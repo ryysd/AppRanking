297bbf9b8ced9d978dee14d0bde58bf9da2f14d2
ryysd <ry.ysd01@gmail.com>
2014-04-04 02:03:17 +0900

    fix db bug

--- db/migrate/20140322021908_rename_fk.rb
+++ db/migrate/20140322021908_rename_fk.rb
@@ -2,7 +2,7 @@ class RenameFk < ActiveRecord::Migration
   def change
       rename_table :device_has_applications, :device_has_app_items
       rename_column :device_has_app_items, :application_id, :app_item_id
-      rename_column :nationals, :application_id, :app_item_id
+      # rename_column :nationals, :application_id, :app_item_id
       rename_column :app_items, :application_id, :app_item_id
   end
 end
